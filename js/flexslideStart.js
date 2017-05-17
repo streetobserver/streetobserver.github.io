cbf3c240b2eb4b0d6f6e3fa2bd49b54e5d4341ad
chris <dwatow@gmail.com>
2017-05-17 16:19:51 +0800

    modify beef souop, change carousel

---
+++ js/flexslideStart.js
@@ -0,0 +1,9 @@
+// Can also be used with $(document).ready()
+$(document)
+	.ready(function () {
+		$('.flexslider')
+			.flexslider({
+				animation: "slide",
+				slideshowSpeed: 3000
+			});
+	});
