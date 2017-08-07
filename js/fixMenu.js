2094dd2dee7e24155deb9364d98a6b20ab82d26a
chris <dwatow@gmail.com>
2017-08-07 10:03:48 +0800

    resort code

--- js/fixMenu.js
+++ js/fixMenu.js
@@ -1,12 +1,14 @@
-$(document).scroll(fixedNav);
+$(document).scroll(sticky);
 
-function fixedNav() {
-  const menuButton = $('#fix_menu_spacer').offset().top - $('nav').outerHeight();
-  console.log(menuButton, window.scrollY);
-  if(window.scrollY > menuButton) {
-    $('nav').addClass('fix_menu');
-  }
-  else {
-    $('nav').removeClass('fix_menu');
-  }
+function sticky() {
+    const direct = 'top';
+    const target_element = $('nav');
+    const menuButton = $('#fix_menu_spacer').offset()[direct] - $('nav').outerHeight();
+    // console.log(menuButton, window.scrollY);
+    if(window.scrollY - menuButton > 0) {
+        target_element.addClass('fix_menu');
+    }
+    else {
+        target_element.removeClass('fix_menu');
+    }
 }
